3d521d3aed883e8a066f49a142781dfc570c8aa5
frederikKoch <fr.koch@live.de>
2023-01-27 17:18:16 -0500

    Modularized writing the parameters into the output file

--- wave1d.cpp
+++ wave1d.cpp
@@ -89,6 +89,22 @@ void deriveParameters(Parameters &param){
     param.nper   = static_cast<size_t>(param.outtime/param.dt);      // how many steps between snapshots (rounded down)
 };
 
+void writeParameters(Parameters &param, std::ofstream &fout){
+    // Report all the parameters in the output file (prepend # to facilitate post-processing, as e.g. gnuplot and numpy.loadtxt skip these)
+    fout << "#c        " << param.c       << "\n";
+    fout << "#tau      " << param.tau     << "\n";
+    fout << "#x1       " << param.x1      << "\n";
+    fout << "#x2       " << param.x2      << "\n";
+    fout << "#runtime  " << param.runtime << "\n";
+    fout << "#dx       " << param.dx      << "\n";
+    fout << "#outtime  " << param.outtime << "\n";
+    fout << "#filename " << param.outfilename << "\n"; 
+    fout << "#ngrid (derived) " << param.ngrid  << "\n";
+    fout << "#dt    (derived) " << param.dt     << "\n";
+    fout << "#nsteps(derived) " << param.nsteps << "\n";    
+    fout << "#nper  (derived) " << param.nper   << "\n";
+};
+
 int main(int argc, char* argv[])
 {
     // Check command line argument
@@ -106,21 +122,8 @@ int main(int argc, char* argv[])
    
     // Open output file
     std::ofstream fout(param.outfilename);
+    writeParameters(param, fout);
     
-    // Report all the parameters in the output file (prepend # to facilitate post-processing, as e.g. gnuplot and numpy.loadtxt skip these)
-    fout << "#c        " << param.c       << "\n";
-    fout << "#tau      " << param.tau     << "\n";
-    fout << "#x1       " << param.x1      << "\n";
-    fout << "#x2       " << param.x2      << "\n";
-    fout << "#runtime  " << param.runtime << "\n";
-    fout << "#dx       " << param.dx      << "\n";
-    fout << "#outtime  " << param.outtime << "\n";
-    fout << "#filename " << param.outfilename << "\n"; 
-    fout << "#ngrid (derived) " << param.ngrid  << "\n";
-    fout << "#dt    (derived) " << param.dt     << "\n";
-    fout << "#nsteps(derived) " << param.nsteps << "\n";    
-    fout << "#nper  (derived) " << param.nper   << "\n";
-
     // Define and allocate arrays
     auto rho_prev = std::make_unique<double[]>(param.ngrid); // time step t-1
     auto rho      = std::make_unique<double[]>(param.ngrid); // time step t
