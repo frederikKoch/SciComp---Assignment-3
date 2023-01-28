58d97dbf39642c12bf82855d7b7e4bef6c2082fa
frederikKoch <fr.koch@live.de>
2023-01-28 14:44:47 -0500

    Put all file interaction into fileInteraction.cpp. Modified Makefile accordingly. Everything works

--- Makefile
+++ Makefile
@@ -5,11 +5,14 @@ CXXFLAGS=-O2 -g -std=c++17 -Wall -Wfatal-errors -Wconversion
 LDFLAGS=-O2 -g
 all: wave1d
 
-wave1d: wave1d.o
-	$(CXX) $(LDFLAGS) -o wave1d wave1d.o
+wave1d: wave1d.o fileInteraction.o
+	$(CXX) $(LDFLAGS) -o wave1d wave1d.o fileInteraction.o
 
 wave1d.o: wave1d.cpp wave1d.h
 	$(CXX) -c $(CXXFLAGS) -o wave1d.o wave1d.cpp
+
+fileInteraction.o: fileInteraction.cpp wave1d.h
+	$(CXX) -c $(CXXFLAGS) -o fileInteraction.o fileInteraction.cpp
 
 run: wave1d
 	./wave1d waveparams.txt
--- wave1d.cpp
+++ wave1d.cpp
@@ -13,74 +13,6 @@
 #include <vector>
 #include "wave1d.h"
 
-Parameters readFile(std::string filename){
-    // Read the values from the parameter file specified on the command line
-    Parameters    param;
-    std::ifstream infile(filename);
-    // The following line causes 'infile' to throw exceptions for errors.
-    // (instead of the default behavior of setting an internal flag and having the program continue.)
-    infile.exceptions(std::ifstream::failbit|std::ifstream::badbit);  
-    try {
-        infile >> param.c;
-        infile >> param.tau;
-        infile >> param.x1;
-        infile >> param.x2;
-        infile >> param.runtime;
-        infile >> param.dx;
-        infile >> param.outtime;
-        infile >> param.outfilename;
-        infile.close();
-    }
-    catch (std::ifstream::failure& e) {
-        std::cerr << "Error while reading file '" << filename << "'.\n";
-        std::exit(1); 
-    }
-
-    // Check input sanity, quit if there are errors
-    bool correctinput = false ; // assume the worst first.
-    if (param.c <= 0.0) {
-        std::cerr << "wave speed c must be postive.\n";
-    } else if (param.tau <= 0.0) {
-        std::cerr << "damping time tau must be positive or zero\n";
-    } else if (param.x1 >= param.x2) {
-        std::cerr << "x1 must be less that x2.\n";
-    } else if (param.dx < 0) {
-        std::cerr << "dx must be postive.\n";
-    } else if (param.dx > param.x2 - param.x1) {
-        std::cerr << "dx too large for domain.\n";
-    } else if (param.runtime < 0.0) {
-        std::cerr << "runtime must be positive.\n";
-    } else if (param.outtime < 0.0) {
-        std::cerr << "outtime must be positive.\n";
-    } else if (param.outfilename.size() == 0) {
-        std::cerr << "no output filename given.\n";
-    } else {
-        correctinput = true;
-    }
-    if (not correctinput) {
-        std::cerr << "Parameter value error in file '" << filename << "'\n";
-        std::exit(0);
-    }
- 
-    return param;
-};
-
-void writeParameters(Parameters &param, std::ofstream &fout){
-    // Report all the parameters in the output file (prepend # to facilitate post-processing, as e.g. gnuplot and numpy.loadtxt skip these)
-    fout << "#c        " << param.c       << "\n";
-    fout << "#tau      " << param.tau     << "\n";
-    fout << "#x1       " << param.x1      << "\n";
-    fout << "#x2       " << param.x2      << "\n";
-    fout << "#runtime  " << param.runtime << "\n";
-    fout << "#dx       " << param.dx      << "\n";
-    fout << "#outtime  " << param.outtime << "\n";
-    fout << "#filename " << param.outfilename << "\n"; 
-    fout << "#ngrid (derived) " << param.ngrid  << "\n";
-    fout << "#dt    (derived) " << param.dt     << "\n";
-    fout << "#nsteps(derived) " << param.nsteps << "\n";    
-    fout << "#nper  (derived) " << param.nper   << "\n";
-};
-
 std::vector<double> initializeX(Parameters param){
     // Initialize array of x values 
     std::vector<double> x (param.ngrid, 0); 
@@ -103,12 +35,6 @@ std::vector<double> initializeRho(Parameters param, std::vector<double> x){
         }
     }
     return rho;
-};
-
-void printX(std::ofstream &fout, std::vector<double> rho, std::vector<double> x, Parameters param){
-  for (size_t i = 0; i < param.ngrid; i++)  {
-        fout << x[i] << " " << rho[i] << "\n";
-    }
 };
 
 std::vector<double> timeStep(std::vector<double> &rho, std::vector<double> &rho_prev, Parameters param){
